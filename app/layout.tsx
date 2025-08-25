49563569b1ed0873c0cd0eab7f8b436620190ae7
Yuuki Takusi <e215737@ie.u-ryukyu.ac.jp>
2025-08-25 16:58:46 +0900

    add Client/Server Component, footer and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
         </ThemeProvider>
       </body>
